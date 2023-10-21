fd6db357e1634370b4c34ba515971e46026f4b86
mays4 <maysalani84@yahoo.com>
2023-10-20 20:06:03 -0400

    change in code

--- api/index.js
+++ api/index.js
@@ -1,43 +1,52 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const client = new MongoClient(process.env.CONNECTION_STRING, {
+// Connect to the database
+mongoose.connect(process.env.CONNECTION_STRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// Create the schema
+const imageSchema = new mongoose.Schema({
+  title: String,
+  image: {
+    data: Buffer,
+    contentType: String,
+  },
+});
+
+// Create a model using the schema
+const ImageModel = mongoose.model('Image', imageSchema);
+
+// Handle POST requests to save the image
 app.post('/saveImage', async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db('your-database-name');
-    const collection = database.collection('your-collection-name');
-
     const { title, imageData } = req.body;
 
-    const image = {
+    const image = new ImageModel({
       title: title,
       image: {
         data: Buffer.from(imageData, 'base64'),
-        contentType: 'image/jpeg' // Adjust this based on the image type
-      }
-    };
+        contentType: 'image/jpeg', // Adjust this based on the image type
+      },
+    });
 
-    const result = await collection.insertOne(image);
-    console.log(`Image inserted with id: ${result.insertedId}`);
+    await image.save();
+    console.log(`Image inserted with id: ${image._id}`);
     res.status(200).send('Image saved to MongoDB');
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
-  } finally {
-    await client.close();
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
