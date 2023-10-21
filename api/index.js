a67f153bfaab543b4fdc43298da59239e23b2531
mays4 <maysalani84@yahoo.com>
2023-10-20 20:22:52 -0400

    change the logic of the code

--- api/index.js
+++ api/index.js
@@ -8,17 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // Connect to the database
 mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // Create the schema
 const imageSchema = new mongoose.Schema({
-  title: String,
-  image: {
-    data: Buffer,
-    contentType: String,
-  },
+    title: String,
+    image: {
+        data: Buffer,
+        contentType: String,
+    },
 });
 
 // Create a model using the schema
@@ -26,27 +26,29 @@ const ImageModel = mongoose.model('Image', imageSchema);
 
 // Handle POST requests to save the image
 app.post('/saveImage', async (req, res) => {
-  try {
-    const { title, imageData } = req.body;
+    try {
+        const imageData = req.files.file;
+        const image = new ImageModel({
+            title: 'Uploaded Image',
+            image: {
+                data: imageData.data,
+                contentType: imageData.mimetype,
+            },
+        });
 
-    const image = new ImageModel({
-      title: title,
-      image: {
-        data: Buffer.from(imageData, 'base64'),
-        contentType: 'image/jpeg', // Adjust this based on the image type
-      },
-    });
-
-    await image.save();
-    console.log(`Image inserted with id: ${image._id}`);
-    res.status(200).send('Image saved to MongoDB');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+        await image.save();
+        console.log(`Image inserted with id: ${image._id}`);
+        res.status(200).json({
+            message: 'Image saved to MongoDB',
+            imageSrc: `data:${image.image.contentType};base64,${image.image.data.toString('base64')}`,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- public/local-index.js
+++ public/local-index.js
@@ -1,24 +1,34 @@
+async function fetchImage(imageId) {
+  try {
+    const response = await fetch(`/getImage/${imageId}`);
+    const blob = await response.blob();
+    const imageContainer = document.getElementById('imageContainer');
+    const imagePreview = document.getElementById('imagePreview');
+    imagePreview.src = URL.createObjectURL(blob);
+    imageContainer.style.display = 'block';
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
 async function submitToDB(event) {
   event.preventDefault();
-  let confirmation = confirm("Confirm submit to MongoDB?");
+  let confirmation = confirm('Confirm submit to MongoDB?');
   if (confirmation) {
-      const fileInput = document.getElementById('myFile');
-      const formData = new FormData();
-      formData.append('file', fileInput.files[0]);
+    const fileInput = document.getElementById('myFile');
+    const formData = new FormData();
+    formData.append('file', fileInput.files[0]);
 
-      try {
-          const response = await fetch('/saveImage', {
-              method: 'POST',
-              body: formData
-          });
-          const data = await response.json();
-          const imageContainer = document.getElementById('imageContainer');
-          const imagePreview = document.getElementById('imagePreview');
-          imagePreview.src = data.imageSrc;
-          imageContainer.style.display = 'block';
-          alert(data.message);
-      } catch (error) {
-          console.error('Error:', error);
-      }
+    try {
+      const response = await fetch('/saveImage', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      fetchImage(data.imageId);
+      alert(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 }
